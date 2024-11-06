49754df5498d1e95fd96f1b3a07667b3254837d2
Benjamin De Kosnik <bkoz@gnu.org>
2024-11-05 22:29:05 -0800

    add windows, linux, android results with standardized output names

--- scripts/get-treeherder-completed-pageload-jobs.sh
+++ scripts/get-treeherder-completed-pageload-jobs.sh
@@ -1,1 +1,14 @@
-wget https://sql.telemetry.mozilla.org/api/queries/82527/results.json?api_key=wNcI8h4NXXzSHGCnROXPwqnnBfGF6aiKiYFWWGTV
+#!/usr/bin/env bash
+
+ISODATE=`date --iso`
+
+LINUX_URL="https://sql.telemetry.mozilla.org/api/queries/103481/results.json?api_key=gGZtKjMWvDxX1ZowDO1ylPLpb7geLfydJOfPwacA"
+WIN_URL="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
+DROID_URL="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
+
+
+curl --fail --silent --show-error "${LINUX_URL}" --output "$ISODATE-linux.json";
+
+curl --fail --silent --show-error "${WIN_URL}" --output "$ISODATE-win11.json";
+
+curl --fail --silent --show-error "${DROID_URL}" --output "$ISODATE-android.json";
