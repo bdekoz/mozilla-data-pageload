7949cc249dcbb0a03d9911ed38d132d0416ea46e
Benjamin De Kosnik <bdekosnik@mozilla.com>
2024-11-06 17:11:17 -0800

    per-site api endpoints for chrome and firefox

--- scripts/get-treeherder-completed-pageload-jobs.sh
+++ scripts/get-treeherder-completed-pageload-jobs.sh
@@ -2,16 +2,29 @@
 
 ISODATE=`date --iso`
 
-LINUX_URL="https://sql.telemetry.mozilla.org/api/queries/103481/results.json?api_key=gGZtKjMWvDxX1ZowDO1ylPLpb7geLfydJOfPwacA"
-WIN_URL="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
-DROID_URL="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
+# Chrome results.
 
+LINUX_URLC="https://sql.telemetry.mozilla.org/api/queries/103481/results.json?api_key=gGZtKjMWvDxX1ZowDO1ylPLpb7geLfydJOfPwacA"
+WIN_URLC="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
+DROID_URLC="https://sql.telemetry.mozilla.org/api/queries/103544/results.json?api_key=77QUfZNd45ckUuoTDv9thesptPNWyE2KC2rzdVTz"
 
-curl --fail --silent --show-error "${LINUX_URL}" --output "$ISODATE-linux.json";
+curl --fail --silent --show-error "${LINUX_URLC}" --output "$ISODATE-linux-chrome.json";
 
-curl --fail --silent --show-error "${WIN_URL}" --output "$ISODATE-win11.json";
+curl --fail --silent --show-error "${WIN_URLC}" --output "$ISODATE-win11-chrome.json";
 
-curl --fail --silent --show-error "${DROID_URL}" --output "$ISODATE-android.json";
+curl --fail --silent --show-error "${DROID_URLC}" --output "$ISODATE-android-chrome.json";
+
+# Firefox results.
+
+LINUX_URLF="https://sql.telemetry.mozilla.org/api/queries/103627/results.json?api_key=wQwoUqUlDvCba60ir76jzS65jVktTRDfh2JLXktx"
+WIN_URLF="https://sql.telemetry.mozilla.org/api/queries/103629/results.json?api_key=0vQ5Fr96IWnh13o30sVeJhNnjPTyh5itBX3mQIu3"
+DROID_URLF="https://sql.telemetry.mozilla.org/api/queries/103628/results.json?api_key=A6nFeIVdQtW4lCwHS01CaL3n7nA6e6LbUqrUiGjy"
+
+curl --fail --silent --show-error "${LINUX_URLF}" --output "$ISODATE-linux-firefox.json";
+
+curl --fail --silent --show-error "${WIN_URLF}" --output "$ISODATE-win11-firefox.json";
+
+curl --fail --silent --show-error "${DROID_URLF}" --output "$ISODATE-android-firefox.json";
 
 
 #task_id is LrntcFBZSBW0fxJXJIlTxg
