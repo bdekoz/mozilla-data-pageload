6c17053fcdd1f36ffbaed2c0625a51073e987e8b
Benjamin De Kosnik <bkoz@gnu.org>
2024-11-17 12:49:55 -0800

    add 2011-11-11 artifact

--- scripts/get-treeherder-task-artifact.sh
+++ scripts/get-treeherder-task-artifact.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #ISODATE=`date --iso`
-ISODATE=2024-11-13
+ISODATE=2024-11-11
 
 get_artifact_and_unpack() {
     TPLATFORM="$1"
@@ -32,10 +32,19 @@ get_artifact_and_unpack() {
 
 # 2024-11-13
 # revision: 723946b9a47990aa6253585366bb18863de4df33
-get_artifact_and_unpack "android-chrome-amazon" "TZYbceDiQmi9QFRD-p5SFA"
-get_artifact_and_unpack "android-firefox-amazon" "SRBmXxxqQgqdh93HmOnarA"
-get_artifact_and_unpack "linux-chrome-amazon" ""
-get_artifact_and_unpack "linux-firefox-amazon" "TL0sHKldQ1e_yCOZnRqwAQ"
-get_artifact_and_unpack "win11-chrome-amazon" "Oa70G72URmi3IWnVPCBjfA"
-get_artifact_and_unpack "win11-firefox-amazon" "HJZ7SBoBT_Cm27D5w05_3g"
+#get_artifact_and_unpack "android-chrome-amazon" "TZYbceDiQmi9QFRD-p5SFA"
+#get_artifact_and_unpack "android-firefox-amazon" "SRBmXxxqQgqdh93HmOnarA"
+#get_artifact_and_unpack "linux-chrome-amazon" ""
+#get_artifact_and_unpack "linux-firefox-amazon" "TL0sHKldQ1e_yCOZnRqwAQ"
+#get_artifact_and_unpack "win11-chrome-amazon" "Oa70G72URmi3IWnVPCBjfA"
+#get_artifact_and_unpack "win11-firefox-amazon" "HJZ7SBoBT_Cm27D5w05_3g"
+
+# 2024-11-11
+# revision: 544768c159ebb03a8419e5fe3b1135bbce4965a5 amarc@mozilla.com
+get_artifact_and_unpack "android-chrome-amazon" "XVtnHMq5RIKSjctXmONWUA"
+get_artifact_and_unpack "android-firefox-amazon" "FY3DkYW_TqK-OAAnRWUCag"
+get_artifact_and_unpack "linux-chrome-amazon" "bSrPsvpRRyuV8Viufi2ThA"
+get_artifact_and_unpack "linux-firefox-amazon" "MK_AQzRzTxicH4mqSoLwmA"
+get_artifact_and_unpack "win11-chrome-amazon" "AMQmRGlnQECayEI5w7AFzw"
+get_artifact_and_unpack "win11-firefox-amazon" "ac68E6Y1R5eYweHTaGtNEQ"
 
