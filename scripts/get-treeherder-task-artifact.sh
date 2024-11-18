086a2a833af527147101e56a6073ca3e349a4b0d
Benjamin De Kosnik <bdekosnik@mozilla.com>
2024-11-17 22:50:53 -0800

    2024-11-15 android results

--- scripts/get-treeherder-task-artifact.sh
+++ scripts/get-treeherder-task-artifact.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #ISODATE=`date --iso`
-ISODATE=2024-11-11
+ISODATE=2024-11-15
 
 get_artifact_and_unpack() {
     TPLATFORM="$1"
@@ -30,6 +30,15 @@ get_artifact_and_unpack() {
 }
 
 
+# 2024-11-11
+# revision: 544768c159ebb03a8419e5fe3b1135bbce4965a5 amarc@mozilla.com
+#get_artifact_and_unpack "android-chrome-amazon" "XVtnHMq5RIKSjctXmONWUA"
+#get_artifact_and_unpack "android-firefox-amazon" "FY3DkYW_TqK-OAAnRWUCag"
+#get_artifact_and_unpack "linux-chrome-amazon" "bSrPsvpRRyuV8Viufi2ThA"
+#get_artifact_and_unpack "linux-firefox-amazon" "MK_AQzRzTxicH4mqSoLwmA"
+#get_artifact_and_unpack "win11-chrome-amazon" "AMQmRGlnQECayEI5w7AFzw"
+#get_artifact_and_unpack "win11-firefox-amazon" "ac68E6Y1R5eYweHTaGtNEQ"
+
 # 2024-11-13
 # revision: 723946b9a47990aa6253585366bb18863de4df33
 #get_artifact_and_unpack "android-chrome-amazon" "TZYbceDiQmi9QFRD-p5SFA"
@@ -39,12 +48,16 @@ get_artifact_and_unpack() {
 #get_artifact_and_unpack "win11-chrome-amazon" "Oa70G72URmi3IWnVPCBjfA"
 #get_artifact_and_unpack "win11-firefox-amazon" "HJZ7SBoBT_Cm27D5w05_3g"
 
-# 2024-11-11
-# revision: 544768c159ebb03a8419e5fe3b1135bbce4965a5 amarc@mozilla.com
-get_artifact_and_unpack "android-chrome-amazon" "XVtnHMq5RIKSjctXmONWUA"
-get_artifact_and_unpack "android-firefox-amazon" "FY3DkYW_TqK-OAAnRWUCag"
-get_artifact_and_unpack "linux-chrome-amazon" "bSrPsvpRRyuV8Viufi2ThA"
-get_artifact_and_unpack "linux-firefox-amazon" "MK_AQzRzTxicH4mqSoLwmA"
-get_artifact_and_unpack "win11-chrome-amazon" "AMQmRGlnQECayEI5w7AFzw"
-get_artifact_and_unpack "win11-firefox-amazon" "ac68E6Y1R5eYweHTaGtNEQ"
+# 2024-11-17
+
+# 2024-11-15T09:34:47Z
+# revision: f918befd42312cdf96757bea838a55620ce4cb7f
+# pstanciu@mozilla.com, id(482802694), push_id(1533676)
+get_artifact_and_unpack "android-chrome-amazon" "AiNhygfUTBC1JigtAFejKg"
+get_artifact_and_unpack "android-firefox-amazon" "Janmma67QIiH7tIK-RXgyQ"
+
+#get_artifact_and_unpack "linux-chrome-amazon" ""
+#get_artifact_and_unpack "linux-firefox-amazon" ""
+#get_artifact_and_unpack "win11-chrome-amazon" ""
+#get_artifact_and_unpack "win11-firefox-amazon" ""
 
