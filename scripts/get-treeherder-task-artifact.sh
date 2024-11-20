2eb6048523191f9ec08271621fd65b7d9858b5ca
Benjamin De Kosnik <bdekosnik@mozilla.com>
2024-11-19 22:42:15 -0800

    use closest iteration to find artifact

--- scripts/get-treeherder-task-artifact.sh
+++ scripts/get-treeherder-task-artifact.sh
@@ -3,6 +3,8 @@
 #ISODATE=`date --iso`
 ISODATE=2024-11-15
 
+ITERATIONFINDER=$MOZPERFAX/bin/moz-perf-x-extract.browsertime_iteration.exe
+
 get_artifact_and_unpack() {
     TPLATFORM="$1"
     TASKID="$2"
@@ -18,15 +20,26 @@ get_artifact_and_unpack() {
     # select data files and copy/rename.
     ARTIFACT_BASE="$ISODATE-$TPLATFORM";
 
+    # find browsertime result json file
     ARTIFACT1_NAME=cold-browsertime.json
+    ARTIFACT1_ONAME=${ARTIFACT_BASE}-${ARTIFACT1_NAME};
     ARTIFACT1=`find ./$ARTIFACT -type f -name $ARTIFACT1_NAME`
-    cp ${ARTIFACT1} ./${ARTIFACT_BASE}-${ARTIFACT1_NAME};
+    cp ${ARTIFACT1} ./${ARTIFACT1_ONAME};
 
-    ARTIFACT2_NAME=1-original.mp4
+    # use it to select iteration to use for rest of extraction.
+    # Note browsertime numbering starts at 0, artifact numbering starts at 1
+    ITER=`$ITERATIONFINDER ./${ARTIFACT1_ONAME}`
+    ITER=$((ITER + 1))
+
+    # find video file.
+    ARTIFACT2_NAME=${ITER}-original.mp4
     ARTIFACT2=`find ./$ARTIFACT -type f -name $ARTIFACT2_NAME | grep cold`
     cp ${ARTIFACT2} ./${ARTIFACT_BASE}.mp4;
 
-    rm -rf ${ARTIFACT} ${ARTIFACT}.tgz
+    #rm -rf ${ARTIFACT} ${ARTIFACT}.tgz
+
+    rm -rf ${ARTIFACT}
+    mv ${ARTIFACT}.tgz ${ARTIFACT_BASE}-${ARTIFACT}.tgz
 }
 
 
@@ -48,16 +61,17 @@ get_artifact_and_unpack() {
 #get_artifact_and_unpack "win11-chrome-amazon" "Oa70G72URmi3IWnVPCBjfA"
 #get_artifact_and_unpack "win11-firefox-amazon" "HJZ7SBoBT_Cm27D5w05_3g"
 
-# 2024-11-17
-
 # 2024-11-15T09:34:47Z
 # revision: f918befd42312cdf96757bea838a55620ce4cb7f
 # pstanciu@mozilla.com, id(482802694), push_id(1533676)
 get_artifact_and_unpack "android-chrome-amazon" "AiNhygfUTBC1JigtAFejKg"
 get_artifact_and_unpack "android-firefox-amazon" "Janmma67QIiH7tIK-RXgyQ"
 
+
 #get_artifact_and_unpack "linux-chrome-amazon" ""
 #get_artifact_and_unpack "linux-firefox-amazon" ""
-#get_artifact_and_unpack "win11-chrome-amazon" ""
-#get_artifact_and_unpack "win11-firefox-amazon" ""
 
+# revision: 563a9354bae0035e1cf01227bc28be4a80a7d5e5
+# 2024-11-15 17:15 aglavic@mozilla.com id(482,873,628) push_id(1,534,036)
+#get_artifact_and_unpack "win11-chrome-amazon" "FzHE0G0XTMSAj0rUpJzLmg"
+#get_artifact_and_unpack "win11-firefox-amazon" ""
